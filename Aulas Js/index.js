637bd4a18e3ddf113eed15c0ca7409d1cc4ea302
João Michael - MK <88468371+Maicon-MK@users.noreply.github.com>
2023-04-10 09:14:18 -0300

    Console.log
    Console.log

---
+++ Aulas Js/index.js
@@ -0,0 +1,12 @@
+/*console(Obejto) log (metodo) - isso e um metodo log dentro da função chamada console 
+dentro das () vai uma chamada, pode ser uma variavel (var), um objeto uma função. O objetivo do console.log e mostra o resultado que iremos chamar dentro dessa função.
+ Valores literais como textos e preciso colocar entre '' apas:
+ EX:valores literais console.log('Michael'); resultado Michael;
+ Para chamar o resultado em aspas se parando por elemento ex: (João) e (Michael) Eu teria que envolver em aspas duplas e separar os elementos em aspas simples:
+ EX:valores literais console.log('João', 'Michael'); resultado João, Michael;
+ mostrar o resulato com joao e michael em aspas simples:
+ EX:console.log("joao 'michael'") resultado joao 'michael';
+ */
+console.log('joao');
+console.log("João 'michael'");
+console.log('joao "michael"');  
